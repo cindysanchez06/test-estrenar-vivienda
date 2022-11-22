8653c6849661953b0f0ea14e79ce68e235f89f3f
Cindy Sanchez <cindyk_98@hotmail.com>
2022-11-21 23:51:22 -0500

    nuevo modulo de API

---
+++ web/modules/custom/api/src/Services/RequestService.php
@@ -0,0 +1,24 @@
+<?php
+
+class RequestService
+{
+    private $client;
+
+    public function __construct()
+    {
+        $this->client = \Drupal::httpClient();
+    }
+
+    public function requests(string $method, string $url, array $headers = [], array $body = []): array
+    {
+        $response = $this->client->request(
+            $method,
+            $url,
+            [
+                'headers' => $headers,
+                'form_params' => $body
+            ]
+        );
+        return json_decode($response->getBody(), TRUE);
+    }
+}
